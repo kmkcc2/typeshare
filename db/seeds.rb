ea912a142150fb6b6fcf08eaa2aab271390cae77
kmkcc2 <kmk.krowicki@gmail.com>
2022-11-03 11:47:43 +0100

    added seed and model dependencies

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create(email: "test@example.com", password: "test123", full_name: "Jim Halpert")
+User.create(email: "hais@enberg.com", password: "test123", full_name: "Walter White")
+User.create(email: "f@riend.com", password: "test123", full_name: "Chanandler Bong")
+
+Tweet.create(body: "Do you guys have som pseudo for sell?", user_id: User.find_by(email: "hais@enberg.com").id)
+Comment.create(text: "let me check. I have lots of pills...", tweet_id: 1, user_id: User.find_by(email: "f@riend.com").id)
+Comment.create(text: "Your pills are swimming in jelly! XDDD", tweet_id: 1, user_id: User.find_by(email: "test@example.com").id)
